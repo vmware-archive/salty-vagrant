b6b2234d44ab52ccd1de7a551fb79b44bb0670ef
Alec Koumjian <akoumjian@gmail.com>
2012-06-11 12:16:14 -0700

    upload configs instead of sharing folder

--- Vagrantfile
+++ Vagrantfile
@@ -2,9 +2,8 @@ require './salt_provisioner.rb'
 
 Vagrant::Config.run do |config|
   config.vm.box = "precise64"
-  config.vm.provision SaltProvisioner do |salt|
-    salt.minion_config = "minion"
-    salt.salt_config_path = "salt/config"
-    salt.salt_file_root_path = "salt/file_root"
-  end
+  config.vm.provision SaltProvisioner
+  # config.vm.provision SaltProvisioner do |salt|
+  #   salt.salt_file_root_path = "salt/file_root"
+  # end
 end
--- salt_provisioner.rb
+++ salt_provisioner.rb
@@ -2,25 +2,24 @@ class SaltProvisioner < Vagrant::Provisioners::Base
 
   class Config < Vagrant::Config::Base
     attr_accessor :minion_config
-    attr_accessor :salt_config_path
-    attr_accessor :salt_config_guest_path
+    attr_accessor :local_master
+    attr_accessor :master_config
     attr_accessor :salt_file_root_path
     attr_accessor :salt_file_root_guest_path
+    attr_accessor :salt_pillar_root_path
+    attr_accessor :salt_pillar_root_guest_path
 
-    def minion_config; @minion_config || "minion"; end
-    def salt_config_path; @salt_config_path || "salt/config"; end
-    def salt_config_guest_path; @salt_config_guest_path || "/etc/salt/"; end
-    def salt_file_root_path; @salt_file_root_path || "salt/file_root"; end
-    def salt_file_root_guest_path; @salt_file_root_guest_path || "/srv/"; end
 
-    # Returns the manifests path expanded relative to the root path of the
-    # environment.
-    def expanded_salt_config_path(root_path)
-      Pathname.new(salt_config_path).expand_path(root_path)
-    end
+    def minion_config; @minion_config || "salt/minion"; end
+    def local_master; @local_master || false; end
+    def master_config; @master_config || "salt/master"; end
+    def salt_file_root_path; @salt_file_root_path || "salt/roots/salt"; end
+    def salt_file_root_guest_path; @salt_file_root_guest_path || "/srv/salt"; end
+    def salt_pillar_root_path; @salt_file_root_path || "salt/roots/pillar"; end
+    def salt_pillar_root_guest_path; @salt_file_root_guest_path || "/srv/pillar"; end
 
-    def expanded_salt_file_root_path(root_path)
-      Pathname.new(salt_file_root_path).expand_path(root_path)
+    def expanded_path(root_path, rel_path)
+      Pathname.new(rel_path).expand_path(root_path)
     end
   end
 
@@ -28,57 +27,76 @@ class SaltProvisioner < Vagrant::Provisioners::Base
     Config
   end
 
-  def share_salt_config_path
-    env[:vm].config.vm.share_folder("salt_config", config.salt_config_guest_path, @expanded_salt_config_path)
+  def prepare
+    # Calculate the paths we're going to use based on the environment
+    @expanded_minion_config_path = config.expanded_path(env[:root_path], config.minion_config)
+
+    if config.use_master
+      @expanded_master_config_path = config.expanded_path(env[:root_path], config.master_config)
+      @expanded_salt_file_root_path = config.expanded_path(env[:root_path], config.salt_file_root_path)
+      @expanded_salt_pillar_root_path = config.expanded_path(env[:root_path], config.salt_pillar_root_path)
+      share_salt_file_root_path
+      share_salt_pillar_root_path
+    end
   end
 
   def share_salt_file_root_path
     env[:vm].config.vm.share_folder("salt_file_root", config.salt_file_root_guest_path, @expanded_salt_file_root_path)
   end
 
+  def share_salt_pillar_root_path
+    env[:vm].config.vm.share_folder("salt_pillar_root", config.salt_pillar_root_guest_path, @expanded_salt_pillar_root_path)
+  end
+
   def add_salt_repo
+    env[:ui].info "Adding Salt PPA."
     env[:vm].channel.sudo("apt-get -q -y install python-software-properties")
     env[:vm].channel.sudo("add-apt-repository -y ppa:saltstack/salt")
     env[:vm].channel.sudo("apt-get -q -y update")
   end
 
   def install_salt_master
+    env[:ui].info "Installing salt master."
     env[:vm].channel.sudo("apt-get -q -y install salt-master")
   end
 
   def install_salt_minion
+    env[:ui].info "Installing salt minion."
     env[:vm].channel.sudo("apt-get -q -y install salt-minion")
   end
 
   def accept_minion_key
+    env[:ui].info "Accepting minion key."
     env[:vm].channel.sudo("salt-key -A")
   end
 
   def call_highstate
+    env[:ui].info "Calling state.highstate"
     env[:vm].channel.sudo("salt-call state.highstate")
   end
 
+  def upload_minion_config
+    env[:ui].info "Copying salt minion config to vm."
+    env[:vm].channel.upload(@expanded_minion_config_path, "/etc/salt/minion")
+  end
 
-  def prepare
-    # Calculate the paths we're going to use based on the environment
-    @expanded_salt_config_path = config.expanded_salt_config_path(env[:root_path])
-    @expanded_salt_file_root_path = config.expanded_salt_file_root_path(env[:root_path])
-    share_salt_config_path
-    share_salt_file_root_path
+  def upload_master_config
+    env[:ui].info "Copying salt master config to vm."
+    env[:vm].channel.upload(@expanded_master_config_path, "/etc/salt/master")
   end
 
   def provision!
-    env[:ui].info "Adding Salt PPA."
     add_salt_repo
-    env[:ui].info "Installing salt master."
-    install_salt_master
-    env[:ui].info "Installing salt minion."
+    upload_minion_config
     install_salt_minion
-    env[:ui].info "Accepting minion key."
-    accept_minion_key
-    env[:ui].info "Calling state.highstate"
-    call_highstate
 
+    if config.use_master
+      upload_master_config
+      install_salt_master
+      accept_minion_key
+    end
+
+    call_highstate
   end
 
   def verify_shared_folders(folders)

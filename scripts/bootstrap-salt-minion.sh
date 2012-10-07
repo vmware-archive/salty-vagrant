d92ead058720e72a81906ae940ba804138698d47
Alec Koumjian <akoumjian@gmail.com>
2012-10-06 22:10:57 -0700

    New logging method

--- scripts/bootstrap-salt-minion.sh
+++ scripts/bootstrap-salt-minion.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 usage()
 {
@@ -31,17 +31,14 @@ do
      esac
 done
 
-LOGFILE=/var/log/bootstrap-salt-minion.log 
-
-log() {
-    message="$@"
-    echo $message
-    echo $message >>$LOGFILE
-}
+# Clear and then log all stdout and sterr to $LOGFILE
+>/var/log/bootstrap-salt-minion.log 
+exec >  >(tee -a /var/log/bootstrap-salt-minion.log )
+exec 2> >(tee -a /var/log/bootstrap-salt-minion.log  >&2)
 
 UNAME=`uname`
 if [ "$UNAME" != "Linux" ] ; then
-    log "Sorry, this OS is not supported."
+    echo "Sorry, this OS is not supported."
     exit 1
 fi
 
@@ -49,15 +46,6 @@ set -e
 trap "echo Installation failed." EXIT
 
 if [ "$UNAME" = "Linux" ] ; then
-    do_with_root() {
-        if [ `whoami` = 'root' ] ; then
-            RESULT=$($*)
-            log $RESULT
-        else
-            log "Salt requires root privileges to install. Please re-run this script as root."
-            exit 1
-        fi
-    }
  
     if [ -f /etc/lsb-release ] ; then
         OS=$(lsb_release -si)
@@ -74,80 +62,79 @@ if [ "$UNAME" = "Linux" ] ; then
         OS="Redhat"
         CODENAME=$(cat /etc/redhat-release)
     else
-        log "Unable to install. Could not detect distribution."
+        echo "Unable to install. Could not detect distribution."
         exit 1
     fi
 
-    log "OS: $OS, CODENAME: $CODENAME"
+    echo "OS: $OS, CODENAME: $CODENAME"
 
     if [ $OS = 'Ubuntu' ]; then
         if [ $DEVELOP = 1 ]; then
-            do_with_root mkdir -p /etc/salt/pki
-            do_with_root apt-get update
-            do_with_root apt-get install -y python-software-properties
-            do_with_root echo | add-apt-repository  ppa:saltstack/salt
-            do_with_root apt-get update
-            do_with_root apt-get install -y salt-minion git-core
-            do_with_root rm -rf /usr/share/pyshared/salt*
-            do_with_root rm -rf /usr/bin/salt-*
-            do_with_root mkdir -p /root/git
-            do_with_root rm -rf /root/git/salt
-            do_with_root cd /root/git
-            do_with_root git clone git://github.com/saltstack/salt.git
-            do_with_root cd /root/git/salt
-            do_with_root python setup.py install --install-layout=deb
+            mkdir -p /etc/salt/pki
+            apt-get update
+            apt-get install -y python-software-properties
+            echo | add-apt-repository  ppa:saltstack/salt
+            apt-get update
+            apt-get install -y salt-minion git-core
+            rm -rf /usr/share/pyshared/salt*
+            rm -rf /usr/bin/salt-*
+            mkdir -p /root/git
+            rm -rf /root/git/salt
+            cd /root/git
+            git clone git://github.com/saltstack/salt.git
+            cd /root/git/salt
+            python setup.py install --install-layout=deb
         elif [ $CODENAME = 'oneiric' ]; then
-            log "Installing for $OS $CODENAME."
-            do_with_root apt-get update
-            do_with_root apt-get -y install python-software-properties
-            do_with_root add-apt-repository -y 'deb http://us.archive.ubuntu.com/ubuntu/ oneiric universe'
-            do_with_root add-apt-repository -y ppa:saltstack/salt
-            do_with_root apt-get update
-            do_with_root apt-get -y install msgpack-python salt-minion
-            do_with_root add-apt-repository -y --remove 'deb http://us.archive.ubuntu.com/ubuntu/ oneiric universe'
+            echo "Installing for $OS $CODENAME."
+            apt-get update
+            apt-get -y install python-software-properties
+            add-apt-repository -y 'deb http://us.archive.ubuntu.com/ubuntu/ oneiric universe'
+            add-apt-repository -y ppa:saltstack/salt
+            apt-get update
+            apt-get -y install msgpack-python salt-minion
+            add-apt-repository -y --remove 'deb http://us.archive.ubuntu.com/ubuntu/ oneiric universe'
         elif [ $CODENAME = 'lucid' -o $CODENAME = 'precise' ]; then
-            log "Installing for $OS $CODENAME."
-            do_with_root apt-get update
-            do_with_root apt-get -y install python-software-properties
-            do_with_root add-apt-repository -y ppa:saltstack/salt
-            do_with_root apt-get update
-            do_with_root apt-get -y install salt-minion
+            echo "Installing for $OS $CODENAME."
+            apt-get update
+            apt-get -y install python-software-properties
+            add-apt-repository -y ppa:saltstack/salt
+            apt-get update
+            apt-get -y install salt-minion
         else
-            log "Ubuntu $CODENAME is not supported."
+            echo "Ubuntu $CODENAME is not supported."
             exit 1
         fi
     elif [ $OS = 'Debian' ]; then
         if [ $CODENAME = 'wheezy' -o $CODENAME = 'jessie' ]; then
-            log "Installing for Debian Weezy/Jessie."
-            do_with_root apt-get -y install salt-minion
+            echo "Installing for Debian Weezy/Jessie."
+            apt-get -y install salt-minion
         elif [ $CODENAME = '6.0'  ]; then
-            log "Installing for Debian Squeeze."
-            do_with_root echo "deb http://backports.debian.org/debian-backports squeeze-backports main" >> /etc/apt/sources.list.d/backports.list
-            do_with_root apt-get update
-            do_with_root apt-get -t squeeze-backports -y install salt-minion
+            echo "Installing for Debian Squeeze."
+            echo "deb http://backports.debian.org/debian-backports squeeze-backports main" >> /etc/apt/sources.list.d/backports.list
+            apt-get update
+            apt-get -t squeeze-backports -y install salt-minion
         else
-            log "Debian $CODENAME is not supported."
+            echo "Debian $CODENAME is not supported."
             exit 1
         fi
 
     elif [ $OS = 'Fedora' ] ; then
-        log "Installing for $OS $CODENAME"
-        do_with_root yum install -y salt-minion
-        # Set the minion to start on reboot
+        echo "Installing for $OS $CODENAME"
+        yum install -y salt-minion
 
     elif [ $OS = 'Redhat' ] ; then
-        log "Installing for Redhat/CentOS"
-        log "(this could take a while)"
-        do_with_root rpm -Uvh --force http://mirrors.kernel.org/fedora-epel/6/x86_64/epel-release-6-7.noarch.rpm
-        do_with_root yum update -y
-        do_with_root yum -y install salt-minion --enablerepo=epel-testing
+        echo "Installing for Redhat/CentOS"
+        echo "(this could take a while)"
+        rpm -Uvh --force http://mirrors.kernel.org/fedora-epel/6/x86_64/epel-release-6-7.noarch.rpm
+        yum update -y
+        yum -y install salt-minion --enablerepo=epel-testing
     else
-        log "Unable to install. Bootstap script does not yet support $OS $CODENAME"
+        echo "Unable to install. Bootstap script does not yet support $OS $CODENAME"
 
         exit 1
     fi
 
 fi
 
-log "Salt has been installed!"
+echo "Salt has been installed!"
 trap - EXIT
